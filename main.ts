aae50f11ab4ca66e206ce91843cbaa6fc062675e
Tobias <sch@aeuble.ch>
2025-01-31 17:30:47 +1300

    renaming and command

--- main.ts
+++ main.ts
@@ -15,17 +15,17 @@ const DEFAULT_SETTINGS: PluginSettings = {
     fileEmoji: '📃',
 };
 
-const VIEW_TYPE_CUSTOM = 'custom-view';
+const VIEW_TYPE_OBSIDAN_RPG = 'obsidian-rpg-view';
 
-class CustomView extends ItemView {
+class ObsidianRPGView extends ItemView {
     private currentFolderPath: string = "";
 
-    constructor(leaf: WorkspaceLeaf, private plugin: MyPlugin) {
+    constructor(leaf: WorkspaceLeaf, private plugin: ObsidianRPG) {
         super(leaf);
     }
 
     getViewType() {
-        return VIEW_TYPE_CUSTOM;
+        return VIEW_TYPE_OBSIDAN_RPG;
     }
 
     getDisplayText() {
@@ -182,7 +182,7 @@ class CustomView extends ItemView {
 }
 
 
-export default class MyPlugin extends Plugin {
+export default class ObsidianRPG extends Plugin {
     settings: PluginSettings;
 
     async onload() {
@@ -196,8 +196,8 @@ export default class MyPlugin extends Plugin {
         // document.head.appendChild(link);
 
         this.registerView(
-            VIEW_TYPE_CUSTOM,
-            (leaf) => new CustomView(leaf, this)
+            VIEW_TYPE_OBSIDAN_RPG,
+            (leaf) => new ObsidianRPGView(leaf, this)
         );
 
         this.addSettingTab(new SettingTab(this.app, this));
@@ -208,7 +208,7 @@ export default class MyPlugin extends Plugin {
             // Open a new tab with four columns
             const newLeaf = this.app.workspace.getLeaf(true);
             newLeaf.setViewState({
-                type: VIEW_TYPE_CUSTOM,
+                type: VIEW_TYPE_OBSIDAN_RPG,
                 active: true
             });
         });
@@ -216,8 +216,8 @@ export default class MyPlugin extends Plugin {
         ribbonIconEl.addClass('obsidian-rpg-ribbon-class');
 
         // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-        const statusBarItemEl = this.addStatusBarItem();
-        statusBarItemEl.setText('Status Bar Text');
+        // const statusBarItemEl = this.addStatusBarItem();
+        // statusBarItemEl.setText('Status Bar Text');
 
         // This adds a simple command that can be triggered anywhere
         this.addCommand({
@@ -227,34 +227,34 @@ export default class MyPlugin extends Plugin {
                 new SampleModal(this.app).open();
             }
         });
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
-            editorCallback: (editor: Editor, view: MarkdownView) => {
-                console.log(editor.getSelection());
-                editor.replaceSelection('Sample Editor Command');
-            }
-        });
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
-        this.addCommand({
-            id: 'open-sample-modal-complex',
-            name: 'Open sample modal (complex)',
-            checkCallback: (checking: boolean) => {
-                // Conditions to check
-                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-                if (markdownView) {
-                    // If checking is true, we're simply "checking" if the command can be run.
-                    // If checking is false, then we want to actually perform the operation.
-                    if (!checking) {
-                        new SampleModal(this.app).open();
-                    }
+        // // This adds an editor command that can perform some operation on the current editor instance
+        // this.addCommand({
+        //     id: 'sample-editor-command',
+        //     name: 'Sample editor command',
+        //     editorCallback: (editor: Editor, view: MarkdownView) => {
+        //         console.log(editor.getSelection());
+        //         editor.replaceSelection('Sample Editor Command');
+        //     }
+        // });
+        // // This adds a complex command that can check whether the current state of the app allows execution of the command
+        // this.addCommand({
+        //     id: 'open-sample-modal-complex',
+        //     name: 'Open sample modal (complex)',
+        //     checkCallback: (checking: boolean) => {
+        //         // Conditions to check
+        //         const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        //         if (markdownView) {
+        //             // If checking is true, we're simply "checking" if the command can be run.
+        //             // If checking is false, then we want to actually perform the operation.
+        //             if (!checking) {
+        //                 new SampleModal(this.app).open();
+        //             }
 
-                    // This command will only show up in Command Palette when the check function returns true
-                    return true;
-                }
-            }
-        });
+        //             // This command will only show up in Command Palette when the check function returns true
+        //             return true;
+        //         }
+        //     }
+        // });
 
         // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
         // Using this function will automatically remove the event listener when this plugin is disabled.
@@ -267,7 +267,7 @@ export default class MyPlugin extends Plugin {
     }
 
     onunload() {
-        this.app.workspace.detachLeavesOfType(VIEW_TYPE_CUSTOM);
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE_OBSIDAN_RPG);
     }
 
     async loadSettings() {
@@ -296,9 +296,9 @@ class SampleModal extends Modal {
 }
 
 class SettingTab extends PluginSettingTab {
-    plugin: MyPlugin;
+    plugin: ObsidianRPG;
 
-    constructor(app: App, plugin: MyPlugin) {
+    constructor(app: App, plugin: ObsidianRPG) {
         super(app, plugin);
         this.plugin = plugin;
     }
